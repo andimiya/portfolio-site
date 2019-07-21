530df259bccb25ef035608e1ee67e2897e659ef8
Andrea <takamiya.andrea@gmail.com>
2019-07-21 11:20:22 -0700

    Turning on service worker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './App.css';
-import 'normalize.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./App.css";
+import "normalize.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+serviceWorker.register();
