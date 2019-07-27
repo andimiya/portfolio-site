04c5e80f9a1927fb38ec2d1fc6f8c71b75009cf3
Andrea <takamiya.andrea@gmail.com>
2019-07-26 21:46:01 -0700

    Style updates for app size

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "bulma/css/bulma.css";
 import "./App.scss";
 import "normalize.css";
 import * as serviceWorker from "./serviceWorker";
